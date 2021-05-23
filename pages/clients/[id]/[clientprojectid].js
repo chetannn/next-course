57eee58a5a1fa657ff813676cf0e81d62b3806bd
chetan <chetankharel7@gmail.com>
2021-05-23 08:50:56 +0545

    file-based-routing

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+
+function SelectedClientProjectPage() {
+    const router = useRouter()
+    
+    return (
+        <div>
+            <h1>The project page for a specific project for a selected client</h1>
+        </div>
+    )
+}
+
+export default SelectedClientProjectPage
---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,14 @@
+import {  useRouter, withRouter } from 'next/router'
+
+function PortfolioProjectPage() {
+
+    const router = useRouter()
+    
+    return (
+        <div>
+            <h1>The Porfolio project page</h1>
+            </div>
+    );
+}
+
+export default PortfolioProjectPage
